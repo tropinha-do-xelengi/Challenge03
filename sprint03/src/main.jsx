136cb190ae72558bcbd0d61b18a19d642392a278
NeugeMa <mariana.n.dourado@gmail.com>
2023-09-12 14:53:51 -0300

    adicionamos conteudo dentro do app + apagamos o conteudo main

--- sprint03/src/main.jsx
+++ sprint03/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react' 
+import ReactDOM from 'react-dom/client' 
+import App from './App.jsx' 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Aplicação de rotas 
+
+  //Termino rotas
+
+  
+
+  ReactDOM.createRoot(document.getElementById('root')).render( 
+
+  <React.StrictMode> 
+    <RouterProvider router={router}/> 
+  </React.StrictMode>, 
+
+  ) 
